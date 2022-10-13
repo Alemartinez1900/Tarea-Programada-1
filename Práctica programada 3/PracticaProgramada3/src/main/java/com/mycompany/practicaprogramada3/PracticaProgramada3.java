83a050b5a62f796a888280966f3321111f405774
Alemartinez1900 <113945135+Alemartinez1900@users.noreply.github.com>
2022-10-12 22:49:36 -0600

    Add files via upload

---
+++ Práctica programada 3/PracticaProgramada3/src/main/java/com/mycompany/practicaprogramada3/PracticaProgramada3.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.practicaprogramada3;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PracticaProgramada3 {
+
+    public static void main(String[] args) {
+       Estudiante Estudiante = new Estudiante();
+    
+       Estudiante.NombreEstudiante();
+       Estudiante.edad();
+       Estudiante.nivel();
+       Estudiante.Estatura();
+       Estudiante.sexo();
+       
+       Estudiante.MostrarDatosEstudiante();
+       
+    }   
+    }
